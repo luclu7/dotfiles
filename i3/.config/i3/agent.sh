1a3d1e99fd69762ce7bc3499616aa1bec5b021f3
Luclu7 <me@luclu7.fr>
2020-07-16 16:30:33 +0200

    ssh agent things

---
+++ i3/.config/i3/agent.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if [ -f ~/.ssh/agent.env ] ; then
+    . ~/.ssh/agent.env > /dev/null
+    if ! kill -0 $SSH_AGENT_PID > /dev/null 2>&1; then
+        echo "Stale agent file found. Spawning a new agent. "
+        eval `ssh-agent | tee ~/.ssh/agent.env`
+        ssh-add
+    fi
+else
+    echo "Starting ssh-agent"
+    eval `ssh-agent | tee ~/.ssh/agent.env`
+    ssh-add
+fi
+
